e2d0443b9c2139caf2b2dcb1b70b666dac557e9a
Efigum <efigum@gmail.com>
2022-09-26 17:39:46 -0500

    20220926. Implementacion Frontend 1

--- Mascota_Feliz.App/Mascota_Feliz.App.Presentacion/Pages/Pages_Propietario_Mascota/List.cshtml.cs
+++ Mascota_Feliz.App/Mascota_Feliz.App.Presentacion/Pages/Pages_Propietario_Mascota/List.cshtml.cs
@@ -1,12 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mascota_Feliz.App.Dominio;
+using Mascota_Feliz.App.Persistencia;
+using Mascota_Feliz.App.Persistencia.App_Repositorios;
 
 namespace Mascota_Feliz.App.Presentacion.Pages
 {
     public class ListModel : PageModel
     {
+        private readonly IRepositorio_Propietario_Mascota repositorio_Propietario_Mascota;
+        public IEnumerable<Propietario_Mascota> propietarios{ get; set; }
+
+        public ListModel()
+        {
+            this.repositorio_Propietario_Mascota = new Repositorio_Propietario_Mascota(new Mascota_Feliz.App.Persistencia.App_Repositorios.AppContext());
+        }
+
         public void OnGet()
         {
+            propietarios = repositorio_Propietario_Mascota.GetAllpropietarios();
         }
     }
 }
